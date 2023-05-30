296d8f51c05894d5c11460251d9ad5c7b93a2f3d
筒井公太郎 <newworld@tsutsuikoutarounoMacBook-Air.local>
2023-05-30 18:39:27 +0900

    go-single.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <footer class="l-footer">
-        <h3 class="c-text c-text--footer">ショップ情報｜ヒストリー</h3>
+        <h3 class="c-text c-text--footer"><a href="<?php echo esc_url( home_url('/')); ?>">ショップ情報</a> | <a>ヒストリー</a></h3>
         <p class="c-text c-text--copyright">Copyright: RaiseTech</p>
     </footer>
     <!-- <script src="./JS/style.js"></script> -->
