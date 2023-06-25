4077ff645e0082d379a210e19aa258708352fb77
筒井公太郎 <newworld@tsutsuikoutarounoMacBook-Air.local>
2023-06-25 21:59:17 +0900

    retake-5

--- footer.php
+++ footer.php
@@ -1,11 +1,9 @@
     <footer class="l-footer">
-        <!-- <h3 class="c-text c-text--footer"> -->
             <?php wp_nav_menu(array(
                 'theme_location' => 'shop_menu',
                 'menu_class'    =>  'c-text--footer',
                 ));
             ?>
-        <!-- </h3> -->
         <p class="c-text c-text--copyright">Copyright: RaiseTech</p>
     </footer>
     </div>
